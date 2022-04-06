8cf3d4f5227736647d78a911e2d91770a93cd1e4
leo <leo.ertuna@gmail.com>
2022-04-06 18:45:10 +0300

    Added image inputs

--- tekleo_common_message_protocol/__init__.py
+++ tekleo_common_message_protocol/__init__.py
@@ -1,5 +1,9 @@
+from .image_base64_input import ImageBase64Input
+from .image_url_input import ImageUrlInput
 from .ping_output import PingOutput
 
 __all__ = [
-    'PingOutput',
+    ImageBase64Input,
+    ImageUrlInput,
+    PingOutput,
 ]

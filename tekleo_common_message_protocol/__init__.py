95cafdf363d4f707cb33856579a06615405bcf8d
leo <leo.ertuna@gmail.com>
2022-04-26 00:44:43 +0300

    Switched masks to pixel points

--- tekleo_common_message_protocol/__init__.py
+++ tekleo_common_message_protocol/__init__.py
@@ -1,6 +1,7 @@
 from .image_base64_input import ImageBase64Input
 from .image_url_input import ImageUrlInput
 from .ping_output import PingOutput
+from .point_pixel import PointPixel
 from .rectangle_pixel import RectanglePixel
 from .rectangle_relative import RectangleRelative
 
@@ -13,6 +14,7 @@ __all__ = [
     ImageBase64Input,
     ImageUrlInput,
     PingOutput,
+    PointPixel,
     RectanglePixel,
     RectangleRelative,
 
--- tekleo_common_message_protocol/object_detection/od_prediction.py
+++ tekleo_common_message_protocol/object_detection/od_prediction.py
@@ -1,6 +1,8 @@
+from typing import List
 from pydantic import BaseModel
 from simplestr import gen_str_repr_eq
 from tekleo_common_message_protocol.rectangle_pixel import RectanglePixel
+from tekleo_common_message_protocol.point_pixel import PointPixel
 
 
 @gen_str_repr_eq
@@ -8,6 +10,7 @@ class OdPrediction(BaseModel):
     label: str
     score: float
     region: RectanglePixel
+    mask: List[PointPixel]
 
-    def __init__(self, label: str, score: float, region: RectanglePixel) -> None:
-        super().__init__(label=label, score=score, region=region)
+    def __init__(self, label: str, score: float, region: RectanglePixel, mask: List[PointPixel]) -> None:
+        super().__init__(label=label, score=score, region=region, mask=mask)

a0552f727ec3478da39a48a677f626aae3c2daa0
jpleorx <leo.ertuna@gmail.com>
2022-10-26 21:42:54 +0300

    New message object for relative points and rework of labeled boxes

--- tekleo_common_message_protocol/__init__.py
+++ tekleo_common_message_protocol/__init__.py
@@ -2,13 +2,14 @@ from .image_base64 import ImageBase64
 from .image_url import ImageUrl
 from .ping_output import PingOutput
 from .point_pixel import PointPixel
+from .point_relative import PointRelative
 from .rectangle_pixel import RectanglePixel
 from .rectangle_relative import RectangleRelative
 from .text import Text
 
 from .nlp.nlp_named_entity import NlpNamedEntity
 
-from .object_detection.od_labeled_box import OdLabeledBox
+from .object_detection.od_labeled_item import OdLabeledItem
 from .object_detection.od_output import OdOutput
 from .object_detection.od_prediction import OdPrediction
 from .object_detection.od_sample import OdSample
@@ -19,6 +20,7 @@ __all__ = [
     ImageUrl,
     PingOutput,
     PointPixel,
+    PointRelative,
     RectanglePixel,
     RectangleRelative,
     Text,
@@ -27,7 +29,7 @@ __all__ = [
     NlpNamedEntity,
 
     # Object detection
-    OdLabeledBox,
+    OdLabeledItem,
     OdOutput,
     OdPrediction,
     OdSample,
--- tekleo_common_message_protocol/point_relative.py
+++ tekleo_common_message_protocol/point_relative.py
@@ -3,9 +3,9 @@ from simplestr import gen_str_repr_eq
 
 
 @gen_str_repr_eq
-class PointPixel(BaseModel):
-    x: int # 255 -> 1024
-    y: int
+class PointRelative(BaseModel):
+    x: float # 0.0 - 1.0
+    y: float
 
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x: float, y: float) -> None:
         super().__init__(x=x, y=y)

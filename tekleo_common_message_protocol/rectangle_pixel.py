82c87d4d6949279ab3814f4f78498d8d957bff4c
leo <leo.ertuna@gmail.com>
2023-05-10 21:13:08 +0300

    Methods to get exact points from rectangle

--- tekleo_common_message_protocol/rectangle_pixel.py
+++ tekleo_common_message_protocol/rectangle_pixel.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from simplestr import gen_str_repr_eq
+from tekleo_common_message_protocol.point_pixel import PointPixel
 
 
 @gen_str_repr_eq
@@ -14,3 +15,9 @@ class RectanglePixel(BaseModel):
 
     def get_area(self) -> int:
         return self.w * self.h
+
+    def get_point_top_left(self) -> PointPixel:
+        return PointPixel(self.x, self.y)
+
+    def get_point_bottom_right(self) -> PointPixel:
+        return PointPixel(self.x + self.w, self.y + self.h)
